88dc0a011651989bb2c27bb83840a6c389dddf24
Sören Michaels <soeren.michaels@gmail.com>
2021-05-03 21:36:21 +0200

    removed pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,13 +18,6 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/src/pages`,
-        name: 'pages',
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
         path: `${__dirname}/src/img`,
         name: 'images',
       },
